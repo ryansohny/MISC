69ce0bad9d721b06bb6a43c7f85dbce853079b89
ryansohny <phenomata@gmail.com>
2022-12-02 14:29:10 +0900

    Uploaded Solution to Day02 2022

--- Just_For_Fun/Advent_Of_Code/2022/Day_02/_solution_part2.py
+++ Just_For_Fun/Advent_Of_Code/2022/Day_02/_solution_part2.py
@@ -1,18 +1,18 @@
 #A X
 #A Z
 # A: Rock, B: Paper, C: Scissor
-# X: Rock, Y: Paper, Z: Scissor
+# X: lose, Y: draw, Z: win
 #import os
 #print(os.getcwd())
 
 def score_calculator(List):
     if List[0] == 'A':
         if List[1] == 'X':
-            return 1 + 3
-        elif List[1] == 'Y':
-            return 2 + 6
-        else:
             return 3 + 0
+        elif List[1] == 'Y':
+            return 1 + 3
+        else:
+            return 2 + 6
     elif List[0] == 'B':
         if List[1] == 'X':
             return 1 + 0
@@ -22,13 +22,12 @@ def score_calculator(List):
             return 3 + 6
     else:
         if List[1] == 'X':
-            return 1 + 6
-        elif List[1] == 'Y':
             return 2 + 0
-        else:
+        elif List[1] == 'Y':
             return 3 + 3
-
-
+        else:
+            return 1 + 6
+        
 input_path = "C:\\Users\\mhryansohn.MACROGEN\\Desktop\\GitHub_repo\\MISC\\Just_For_Fun\\Advent_Of_Code\\2022\\Day_02\\"
 
 with open(f"{input_path}input", 'r') as dfh:
@@ -37,5 +36,3 @@ with open(f"{input_path}input", 'r') as dfh:
         game = i.strip().split()
         total_score += score_calculator(game)
     print(total_score)
-    
-# 8890
