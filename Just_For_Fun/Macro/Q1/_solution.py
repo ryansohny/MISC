1e21a13a80b8f57dbfc28c63805b9b921db521f4
ryansohny <phenomata@gmail.com>
2022-12-07 17:15:15 +0900

    asdf

--- Just_For_Fun/Macro/Q1/_solution.py
+++ Just_For_Fun/Macro/Q1/_solution.py
@@ -1,9 +1,19 @@
 import os
-os.chdir("C:\\Users\\mhryansohn.MACROGEN\\Desktop\\GitHub_repo\\MISC\\Macro\\Q1")
-print(os.getcwd())
+os.chdir("C:\\Users\\mhryansohn.MACROGEN\\Desktop\\GitHub_repo\\MISC\\Just_For_Fun\\Macro\\Q1")
 
 with open("gene_list.txt", 'r') as genef:
-    print(genef.readline())
-#import os
-#print(os.getcwd())
-    
+    exp_dic = {}
+    for i in genef:
+        exp_dic[i.strip()] = 'NA'
+
+with open("expression_table.txt") as dfh:
+    dfh.readline()
+    for i in dfh:
+        line = i.strip().split()
+        try:
+            exp_dic[line[0]] += float(line[2])
+        except TypeError:
+            exp_dic[line[0]] = float(line[2])
+
+    for k, v in exp_dic.items():
+        print(k, '\t', v)
